50c32d832ea6eb7088d95b184d64065165e9bec4
JL-J <40110348+JL-J@users.noreply.github.com>
2019-01-10 15:02:56 +0000

    Count 1 to 100

--- FizzBuzzKata/Program.cs
+++ FizzBuzzKata/Program.cs
@@ -10,6 +10,11 @@ namespace FizzBuzzKata
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
